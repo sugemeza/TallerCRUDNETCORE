40aed6d776f9f979145bb8ea041f75d5f83342e2
Davison <davisontobon2019@gmail.com>
2021-03-18 13:19:34 -0500

    SE agrago una clase para enseyar.

---
+++ Models/Ensayo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TallerCRUDNETCORE.Models
+{
+    public class Ensayo
+    {
+    }
+}
